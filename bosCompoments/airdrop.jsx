7717f17ed99858ce7033e10eae71c6472f2c8178
lucasespinosa28 <52639395+lucasespinosa28@users.noreply.github.com>
2023-10-11 01:52:16 -0300

    airdrop

--- bosCompoments/airdrop.jsx
+++ bosCompoments/airdrop.jsx
@@ -1,35 +1,120 @@
+function store() {
+  Storage.set("lastBlockHeight", notifications[0].blockHeight);
+  Storage.get("lastBlockHeight");
+}
+
 return (
-    <div>
-      <h1>Create air drop contract</h1>
-      <p>contract sub name</p>
-      <input onChange={onInputChange} />
-      <p>token contract</p>
-      <input onChange={onInputChange} />
-      <p>NFT contract</p>
-      <input onChange={onInputChange} />
-  
-      <button class="btn btn-primary mt-2" onClick={onBtnClick}>
-        Create
-      </button>
-      <p>Plaea deposit tokens after create airdrop</p>
-      <h1>Set air drop contract</h1>
-      <p>Airdrop account balance</p>
-      <p>NFT total supply</p>
-      <p>Supply Airdrop/NFT</p>
-      <p>Amount of tokes for airdrop</p>
-      <input onChange={onInputChange} />
-      <p>Number of that receive</p>
-      <input onChange={onInputChange} />
-      <p>Total of prize</p>
-      <p>start time</p>
-      <input type="date" onChange={onInputChange} />
-      <p>end time</p>
-      <input type="date" onChange={onInputChange} />
-      <p>list of need banned to airdrops</p>
-      <input type="" onChange={onInputChange} />
-      <button class="btn btn-primary mt-2" onClick={onBtnClick}>
-        Start
-      </button>
+  <div>
+    <h1>Create air drop contract</h1>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        Contract name
+      </span>
+      <input
+        type="text"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
     </div>
-  );
-  
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        Token contract
+      </span>
+      <input
+        type="text"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        NFT contract
+      </span>
+      <input
+        type="text"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
+    </div>
+    <button class="btn btn-primary mt-2" onClick={onBtnClick}>
+      Create
+    </button>
+    <div class="alert alert-primary mt-2">
+      Please deposit token after creating airdrop
+    </div>
+    <hr />
+    <h1>Set air drop contract</h1>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        Airdrop contract id
+      </span>
+      <input
+        type="text"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
+    </div>
+
+    <p>Airdrop account balance</p>
+    <p>NFT total supply</p>
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Amount"
+        aria-label="Amount"
+      />
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Limit of nft"
+        aria-label="Limit of nft"
+      />
+      <span class="input-group-text" id="basic-addon2">
+        tokens per nfts: 1
+      </span>
+    </div>
+
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        Start time
+      </span>
+      <input
+        type="date"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
+    </div>
+    <div class="input-group mb-3">
+      <span class="input-group-text" id="basic-addon1">
+        End time
+      </span>
+      <input
+        type="date"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="Username"
+      />
+    </div>
+    <p>list of need banned to airdrops</p>
+    <div class="alert alert-warning" role="alert">
+      Use "," to create the list or live em white
+    </div>
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        onChange={onInputChange}
+        class="form-control"
+        placeholder="list of banned nfts"
+      />
+    </div>
+    <button class="btn btn-primary mt-2" onClick={onBtnClick}>
+      Start
+    </button>
+  </div>
+);
